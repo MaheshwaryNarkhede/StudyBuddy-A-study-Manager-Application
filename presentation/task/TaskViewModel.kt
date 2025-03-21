3b9b26b58abfb7e2e0144731cef11c7af2f23836
Maheshwary Narkhede <147603457+MaheshwaryNarkhede@users.noreply.github.com>
2025-03-21 11:55:55 +0530

    Update TaskViewModel.kt

--- presentation/task/TaskViewModel.kt
+++ presentation/task/TaskViewModel.kt
@@ -1,4 +1,8 @@
 package com.example.studybuddy.presentation.task
 
-class TaskViewModel {
-}
+data class TaskViewModel(
+    val id: Int,
+    val title: String,
+    val description: String = "",
+    val isCompleted: Boolean = false
+)
